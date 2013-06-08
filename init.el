cd6a274de7c426b564ca6e9be2a04cc83c463101
Mike LaSpina <mike.laspina@gmail.com>
2013-06-08 09:58:59 -0700

    Alternate between first non-WS char and absolute BOL for C-a

--- init.el
+++ init.el
@@ -131,6 +131,17 @@ first level of subdirectories of `basedir'."
       version-control t)      ; use versioned backups
 
 
+;; Smarter BOL movement
+(defadvice move-beginning-of-line (around smarter-bol activate)
+  ;; Move to requested line if needed.
+  (let ((arg (or (ad-get-arg 0) 1)))
+    (when (/= arg 1) (forward-line (1- arg))))
+  ;; Move to indentation on first call, then to actual BOL on second.
+  (let ((pos (point)))
+    (back-to-indentation)
+    (when (= pos (point)) ad-do-it)))
+
+
 ;; Better window movement
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))

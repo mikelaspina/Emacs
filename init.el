f67aec4884f649c75e0c17ed7f8101fe76b618d6
msl <mlaspina@newrelic.com>
2014-10-02 17:39:42 +0200

    Add functions to rename or delete a buffer + file.

--- init.el
+++ init.el
@@ -173,6 +173,38 @@ face attribute."
   (interactive)
   (msl/increment-default-font-height -10))
 
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+
+;; key bindings
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
+(global-set-key (kbd "C-c r")  'rename-file-and-buffer)
+
+
 (defmacro after (mode &rest body)
   "`eval-after-load' MODE evaluate BODY."
   (declare (indent defun))
